9a2559d14606bb03e33f807edfd48ca26f9cc813
gmlwlsdl <heejin094@naver.com>
2024-11-18 11:25:40 +0900

    feat: 헤더 생성
    - 페이지에 따라 헤더 내부에 검색 창이 생김 -> 조건부 렌더링 처리

---
+++ src/components/Header.tsx
@@ -0,0 +1,62 @@
+import Cn from '../utils/cn'
+
+function Header() {
+  return (
+    <>
+      <header className="mt-7 ml-[17px] mr-[19px]">
+        <div className=" flex justify-between">
+          <div className="flex flex-row gap-[10px] items-center">
+            <img
+              src="../src/assets/common/Logo.svg"
+              alt="logoIcon"
+              className="w-9 h-9"
+            />
+            <img
+              src="../src/assets/common/LogoText.svg"
+              alt="logoText"
+              className="w-[37.33px] h-7"
+            />
+          </div>
+          <div className="flex flex-row gap-[14.5px] items-center">
+            <div className="flex gap-2 items-center">
+              <div className="font-SB00 text-[16px]">광진구 능동로 209</div>
+              <img
+                src="../src/assets/common/Arrow.svg"
+                alt="Arrow"
+                className="w-4 h-[6px]"
+              />
+            </div>
+
+            <img
+              src="../src/assets/common/User.svg"
+              alt="userIcon"
+              className="w-[19px] h-[19px]"
+            />
+          </div>
+        </div>
+        {window.location.pathname === '/home' ? (
+          <div className="relative mt-[5.99px] w-full">
+            <input
+              id="searchStore"
+              type="text"
+              placeholder="가맹점 이름을 검색해보세요"
+              className={Cn(
+                'py-[13px] pl-[13px] pr-[27px] w-full font-R00',
+                'border border-200 rounded-lg placeholder:text-200 placeholder:text-sm'
+              )}
+            />
+            <img
+              src="../src/assets/common/Search.svg"
+              alt="searchIcon"
+              className="absolute top-1/2 left-3 transform -translate-y-1/2 pl-[311px]"
+            />
+          </div>
+        ) : (
+          ''
+        )}
+      </header>
+    </>
+  )
+}
+
+export default Header
